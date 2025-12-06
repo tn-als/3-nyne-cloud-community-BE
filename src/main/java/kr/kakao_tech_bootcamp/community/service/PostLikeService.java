62745fdc859a90825d4fb350797bf7cc46f319fe
tn-als <kusumin38@gmail.com>
2025-12-06 20:57:01 +0900

    refactor: fix update likescount

--- src/main/java/kr/kakao_tech_bootcamp/community/service/PostLikeService.java
+++ src/main/java/kr/kakao_tech_bootcamp/community/service/PostLikeService.java
@@ -54,6 +54,8 @@ public class PostLikeService {
 
         postLikeRepository.deleteByUserIdAndPostId(userId, postId);
 
-        return PostLikeResponseDto.of(postId, post.getLikesCount() + postLikeCountManager.getPostLikeCount(postId));
+        int count = post.getLikesCount() - postLikeCountManager.getPostLikeCount(postId);
+
+        return PostLikeResponseDto.of(postId, Math.max(count, 0));
     }
 }
